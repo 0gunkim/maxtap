a15fcddebb4942c59f2dd5776dd7d5303479e497
0gunkim <goldenprevue@gmail.com>
2023-08-29 19:19:55 +0900

    Feat: BookMark component & connect

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template id="app">
+  <ul v-for="bookmark in bookmarks" :key="bookmark.id">
+    <BookMarkItem
+      :id="bookmark.id"
+      :title="bookmark.title"
+      :description="bookmark.description"
+      :link="bookmark.link"
+    />
+  </ul>
+</template>
+<script lang="ts" setup>
+import BookMarkItem from './components/bookmark-list/BookMarkItem.vue'
+import { ref } from 'vue'
+const bookmarks = ref([
+  { id: '1', title: 'vue 공식문서', description: 'Max tap Vue.js', link: 'https://vue.js.org' },
+  { id: '2', title: 'google 공식문서', description: ' google story', link: 'https://google.org' }
+])
+</script>
---
+++ src/components/bookmark-list/BookMarkItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>delete</button>
+      </header>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link" target="_blank">{{ link }}</a>
+      </nav>
+    </div>
+  </li>
+</template>
+<script lang="ts" setup>
+interface BookMarkProps {
+  id: string
+  title: string
+  description: string
+  link: string
+}
+defineProps<BookMarkProps>()
+</script>
+<style scoped></style>
